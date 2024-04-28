32a7b631828eddcf000c0e40c7eddeb787b43a9d
Alanyin <alanyin6@gmail.com>
2024-04-28 16:11:56 +0800

    feat:新增 viem+wagmi 方式访问钱包

--- web/src/components/ConnectButton.tsx
+++ web/src/components/ConnectButton.tsx
@@ -1,7 +1,15 @@
+import { connect } from '@wagmi/core'
+import { injected } from '@wagmi/connectors'
+import { config } from '@/config'
+
 export default function ConnectButton() {
+  const handleConnectWallet = async () => {
+    const result = await connect(config, { connector: injected() })
+  }
+
   return (
     <div>
-      <button>Connect wallet</button>
+      <button onClick={handleConnectWallet}>Connect wallet</button>
     </div>
   );
 }
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -4,11 +4,16 @@ import { ToastContainer } from "react-toastify";
 import "@/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 
+import { WagmiProvider } from 'wagmi' 
+import { config } from '../config' 
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <WagmiProvider config={config}> 
     <Layout>
       <ToastContainer />
       <Component {...pageProps} />
     </Layout>
+    </WagmiProvider>
   );
 }
